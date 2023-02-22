c54fa7857d0fc36a0697789cdea0bdcb112c0663
Kamoteshake <kamoteshake@gmail.com>
2023-02-22 07:43:37 -0600

    feat: update e step

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -976,7 +976,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 424.9}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 420.7} // KS E step calibration
 
 /**
  * Default Max Feed Rate (mm/s)
