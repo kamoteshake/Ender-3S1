2203aa2682de1ac001352e3860921e87512daf33
Kamoteshake <kamoteshake@gmail.com>
2023-03-18 18:43:27 -0500

    feat: M117 implementation attempt

--- Marlin/src/gcode/lcd/M117.cpp
+++ Marlin/src/gcode/lcd/M117.cpp
@@ -27,16 +27,63 @@
 #include "../gcode.h"
 #include "../../lcd/marlinui.h"
 
+#if ENABLED(RTS_AVAILABLE)
+  #include "../../lcd/dwin/lcd_rts.h"
+#endif
+
+void ClearAbnormalPageText() {
+  for (int i = 0; i <= SizeofDatabuf; i++) {
+    rtscheck.RTS_SndData(0, ABNORMAL_PAGE_TEXT_VP + i);
+  }
+}
+
 /**
  * M117: Set LCD Status Message
  */
 void GcodeSuite::M117() {
+  #if ENABLED(RTS_AVAILABLE)
+    if (parser.string_arg && parser.string_arg[0]) {
+      // save the current page to be able to go back to it
+      int previousPage = change_page_font < 1 ? 1 : change_page_font;
+      
+      // change to the abnomal page
+      rtscheck.RTS_SndData(ExchangePageBase + 41, ExchangepageAddr);
+      change_page_font = 41;
 
-  if (parser.string_arg && parser.string_arg[0])
-    ui.set_status(parser.string_arg);
-  else
-    ui.reset_status();
+      // reset status text before using just in case there were an error beforehand
+      ClearAbnormalPageText();
 
+      // format text if it's longer than the text length
+      char statusText[SizeofDatabuf];
+      if (strlen(parser.string_arg) >= SizeofDatabuf) {
+        // copy the string to statusText
+        strncpy(statusText, parser.string_arg, SizeofDatabuf - 1);
+        // change the last 3 chars with ..\0
+        strncpy(&statusText[SizeofDatabuf - 3], "..", 2);
+        statusText[SizeofDatabuf - 1] = '\0';
+      }
+      else {
+        strcpy(statusText, parser.string_arg);
+      }
+
+      // show status text
+      rtscheck.RTS_SndData(statusText, ABNORMAL_PAGE_TEXT_VP);
+
+      delay(3000);
+
+      // return to previous page
+      rtscheck.RTS_SndData(ExchangePageBase + previousPage, ExchangepageAddr);
+      change_page_font = previousPage;
+
+      // reset status text after for the next error handling
+      ClearAbnormalPageText();
+    }
+  #else
+    if (parser.string_arg && parser.string_arg[0])
+      ui.set_status(parser.string_arg);
+    else
+      ui.reset_status();
+  #endif
 }
 
 #endif // HAS_STATUS_MESSAGE
