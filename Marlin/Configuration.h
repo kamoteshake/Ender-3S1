ef66c4c2db8e9679f4c9b51b82801f0c961afe80
Kamoteshake <kamoteshake@gmail.com>
2023-10-21 19:55:52 -0500

    chore: tested single gear extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -976,7 +976,8 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 420.7} // KS E step calibration
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 425.10} // KS E step calibration -- Stock Gear Idler
+// #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 442.66} // KS E step calibration -- Bearing Idler
 
 /**
  * Default Max Feed Rate (mm/s)
